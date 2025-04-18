74fe4a0c1c823ae35f6d4e0dac65f45d4cd1cd69
Arya Vishe <aryavishe@iitb.ac.in>
2025-04-18 13:56:02 +0530

    Testbench works now + dot product
    It still needs a lot of experimenting in order to get the perfect dot
    product but for now we will just continue with this

--- faster_mmul/src/testbench.c
+++ faster_mmul/src/testbench.c
@@ -1,5 +1,6 @@
 #include <signal.h>
 #include <stdio.h>
+#include <time.h>
 #include <stdlib.h>
 #include <stdint.h>
 #include <pthread.h>
@@ -14,29 +15,57 @@
 
 int main(int argc, char* argv[])
 {
+	fprintf(stderr,"Waiting for HW to be interfaced...\n");
 	int I, J;
+	uint32_t A[ORDER][ORDER];
+	uint32_t B[ORDER][ORDER];
+
 	for(I = 0; I < ORDER; I++)
 	{
 		for(J = 0; J < ORDER; J++)
 		{
-			storeA(I, J,(uint32_t)  I+1);
-			storeB(I, J,(uint32_t)  J+1);
+			A[I][J] = I + 1;
+			B[I][J] = J + 1;
+			storeA(I, J,(uint32_t)  A[I][J]);
+			storeB(I, J,(uint32_t)  B[I][J]);
 		}
 	}
 	fprintf(stderr,"Stored A, B\n");
-	
+	fprintf(stderr,"Multiplying now\n");
+
+	clock_t begin = clock();
+
 	mmul();
 
-	fprintf(stderr,"finished dot_product, results:\n");
+	clock_t end = clock();
+	double time_spent = (double)(end - begin) / CLOCKS_PER_SEC;
+	fprintf(stderr,"Time spent: %f seconds\n", time_spent);
+
+	fprintf(stderr,"Finished dot_product, verifying correctness...\n");
 	for(I = 0; I < ORDER; I++)
 	{
 		for(J = 0; J < ORDER; J++)
 		{
+			// First we will multiply A and B ourselves to find C[i][j]
+			uint32_t sum = 0;
+			int k;
+			for(k = 0; k < ORDER; k++)
+			{
+				sum += A[I][k] * B[k][J];
+			}
+
 			uint32_t result = loadC (I,J);
+			
+			if (sum != result){
+				fprintf(stderr, "Noooooooo @ (%d, %d) actual answer = %d but we got back %d\n", I, J, sum, result);
+				return(1);
+			}
+
 			fprintf(stderr,"C[%d][%d] = %d.\n",I, J, result);
 
 		}
 	}
+
 
 	return(0);
 }
